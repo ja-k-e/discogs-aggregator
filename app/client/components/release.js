7cb01dfedcbb11c8a6afd67431157904b55867b3
Jake Albaugh <jake.albaugh@gmail.com>
2018-10-23 11:57:26 -0500

    release page

--- app/client/components/release.js
+++ app/client/components/release.js
@@ -12,44 +12,74 @@ Vue.component("release", {
         .join(" &bull; ");
     }
   },
-  methods: {
-    formatFormat({ name, quantity, description, text }) {
-      let str = `${name}`;
-      if (text) str += `, ${text}`;
-      if (description) str += `, ${description}`;
-      if (quantity !== 1) str += ` (${quantity})`;
-      return str;
-    }
+  data() {
+    return {
+      activeTab: "meta"
+    };
   },
   template: `
   <div>
     <p class="title is-3">{{ release.title }}</p>
     <p class="subtitle is-5">
       <span v-html="formattedArtists"></span> ({{ release.year }})
-      <br> {{ release.formats.map(formatFormat).join(' &bull; ') }}
-      <br> {{ release.labels.map(l => l.name).join(' &bull; ') }}
-      <br>
-      Collections: <strong>{{ release.collection_count }}</strong>
     </p>
-    <br>
 
-    <h2 class="title is-5">Releases often-collected with "{{ release.title }}"</h2>
+    <div class="tabs">
+      <ul>
+        <li :class="{ 'is-active': activeTab == 'meta' }">
+          <a @click="activeTab = 'meta'">Metadata</a>
+        </li>
+        <li :class="{ 'is-active': activeTab == 'common' }">
+          <a @click="activeTab = 'common'">Releases Often Collected with "{{ release.title }}"</a>
+        </li>
+      </ul>
+    </div>
 
-    <table class="table is-narrow is-fullwidth is-striped">
-      <thead>
-        <th><small>Shared Collections</small></th>
-        <th><small>Release</small></th>
-        <th><small>Artists</small></th>
-        <th><small>Labels</small></th>
-      </thead>
-      <tbody>
-        <release-release v-for="r in releases"
-          :key="r.id"
-          :release="r"
-          :collection_count="release.collection_count"
-        ></release-release>
-      </tbody>
-    </table>
+    <div v-if="activeTab == 'meta'">
+      <p class="subtitle is-5">
+        Collections: <strong>{{ release.collection_count }}</strong>
+        <br>
+        Labels: {{ release.labels.map(l => l.name).join(', ') }}
+      </p>
+
+      <div v-if="release.formats">
+        <h2 class="title is-5">Release Formats</h2>
+        <table class="table is-narrow is-fullwidth is-striped">
+          <thead>
+            <th><small>Name</small></th>
+            <th><small>Description</small></th>
+            <th><small>Quantity</small></th>
+            <th><small>Extra</small></th>
+          </thead>
+          <tbody>
+            <tr v-for="f in release.formats">
+              <td v-html="f.name"></td>
+              <td v-html="f.description"></td>
+              <td v-html="f.quantity"></td>
+              <td v-html="f.text"></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div v-if="activeTab == 'common'">
+      <table class="table is-narrow is-fullwidth is-striped">
+        <thead>
+          <th><small>Shared Collections</small></th>
+          <th><small>Release</small></th>
+          <th><small>Artists</small></th>
+          <th><small>Labels</small></th>
+        </thead>
+        <tbody>
+          <release-release v-for="r in releases"
+            :key="r.id"
+            :release="r"
+            :collection_count="release.collection_count"
+          ></release-release>
+        </tbody>
+      </table>
+    </div>
   </div>
   `
 });
